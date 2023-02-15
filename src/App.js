54417faaf612ca3d2f727275dbdad200e6204f86
abhishek <avisec07@gmail.com>
2023-02-15 22:25:29 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import PlayerHome from "./layouts/PlayerHome";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Route path="/" component={<PlayerHome />} exact />
+      </Router>
     </div>
   );
 }
---
+++ src/layouts/PlayerHome.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function () {
+    return (<div>
+            <h1>Player Home Page</h1>
+        </div>);
+}
